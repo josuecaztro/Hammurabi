c66639af4daca437a86958973c522407513e8c8e
Josue <josue@Zipcoders-MacBook-Pro-10.local>
2024-07-12 16:55:25 -0400

    startit

---
+++ docs/matuszek/Hammurabi.java
@@ -0,0 +1,25 @@
+package hammurabi.docs.matuszek;
+import java.util.Random;         // imports go here
+import java.util.Scanner;
+
+public class Hammurabi {         // must save in a file named Hammurabi.java
+    Random rand = new Random();  // this is an instance variable
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) { // required in every Java program
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+
+        int people = 100;
+        int bushels = 2800;
+        int acres = 1000;
+        int landValue = bushels * acres;
+
+        // declare local variables here: grain, population, etc.
+        // statements go after the declations
+    }
+
+    //other methods go here
+}
